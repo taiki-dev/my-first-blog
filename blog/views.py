6d8ae78a6cb8e6d6b052d9295eb8184c593604dd
taiki <taiki.murakoshi.0908@gmail.com>
2020-07-22 11:49:02 +0900

    small change

--- blog/views.py
+++ blog/views.py
@@ -21,7 +21,7 @@ def post_new(request):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
-            return render(request, 'blog/post_edit.html', {'form': form})
+            return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
@@ -36,6 +36,6 @@ def post_edit(request, pk):
             post.published_date =timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
-        else:
-            form = PostForm(instance=post)
-        return render(request, 'blog/post_edit.html', {'form': form})
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
